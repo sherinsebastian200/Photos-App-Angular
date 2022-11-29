548ada82e58cfcf699e9e4723b72231711de0720
sherin200 <sherinsebastian21>
2022-11-29 15:01:19 +0530

    msg

---
+++ src/app/view-photo/view-photo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-photo',
+  templateUrl: './view-photo.component.html',
+  styleUrls: ['./view-photo.component.css']
+})
+export class ViewPhotoComponent {
+  
+  photos:any=[]
+
+}
